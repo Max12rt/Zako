935a8dca4182e2d2317ff8b8e933b074ac5da853
waldemarIT <134318085+waldemarIT@users.noreply.github.com>
2026-05-08 19:29:21 +0200

    feat(frontend): add custom calendar + time picker with month navigation

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderMenu } from './header-menu/header-menu';
+import { DatePicker } from './date-picker/date-picker';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderMenu],
+  imports: [RouterOutlet, HeaderMenu, DatePicker],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {}
+export class App {
+  searchDate: Date = new Date();
+}
